bd868352f8ad0958242dc486c17bf55945a8f072
kirill_10b@mail.ru <kirill_10b@mail.ru>
2020-11-03 14:30:13 +0300

    main class refactoring

--- src/main/java/com/gmail/kirilllapitsky/SpringBootConfig.java
+++ src/main/java/com/gmail/kirilllapitsky/SpringBootConfig.java
@@ -2,14 +2,10 @@ package com.gmail.kirilllapitsky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class SpringBootConfig {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConfig.class, args);
     }
-
 }
