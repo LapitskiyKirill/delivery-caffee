db5e7b706b09c0d307f1959cc7a415fd01b11edd
kirill_10b@mail.ru <kirill_10b@mail.ru>
2020-11-06 16:40:12 +0300

    injection refactoring

--- src/main/java/com/gmail/kirilllapitsky/controller/MenuController.java
+++ src/main/java/com/gmail/kirilllapitsky/controller/MenuController.java
@@ -2,7 +2,6 @@ package com.gmail.kirilllapitsky.controller;
 
 import com.gmail.kirilllapitsky.dto.MenuDto;
 import com.gmail.kirilllapitsky.service.MenuService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("menu")
 public class MenuController {
-    @Autowired
-    private MenuService menuService;
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
 
     @GetMapping("get")
     MenuDto get(@RequestParam Long id) {
-        return  menuService.find(id);
+        return  menuService.findById(id);
     }
 }
--- src/main/java/com/gmail/kirilllapitsky/controller/OfferController.java
+++ src/main/java/com/gmail/kirilllapitsky/controller/OfferController.java
@@ -2,14 +2,19 @@ package com.gmail.kirilllapitsky.controller;
 
 import com.gmail.kirilllapitsky.dto.SpecialOfferDto;
 import com.gmail.kirilllapitsky.service.SpecialOfferService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("specialOffer")
 public class OfferController {
-    @Autowired
-    private SpecialOfferService specialOfferItemsService;
+    private final SpecialOfferService specialOfferItemsService;
+
+    public OfferController(SpecialOfferService specialOfferItemsService) {
+        this.specialOfferItemsService = specialOfferItemsService;
+    }
 
     @GetMapping("get")
     SpecialOfferDto get(@RequestParam Long id) {
