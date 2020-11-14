af6fecfc7d6014d00841ed559e6c45de97f3d42f
kirill_10b@mail.ru <kirill_10b@mail.ru>
2020-11-14 15:09:34 +0300

    #8 Add @NoArgsConstructor to entities

--- src/main/java/com/gmail/kirilllapitsky/entity/MenuItems.java
+++ src/main/java/com/gmail/kirilllapitsky/entity/MenuItems.java
@@ -1,12 +1,14 @@
 package com.gmail.kirilllapitsky.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "menu_items")
 @Data
+@NoArgsConstructor
 public class MenuItems {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
